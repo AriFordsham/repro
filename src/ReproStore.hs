4c355f1b0ce84424f21f27de9cc13a32b85d7fb5
Ari Fordsham <arifordsham@gmail.com>
2023-05-15 15:09:33 +0300

    Refine understanding of derivations

--- src/ReproStore.hs
+++ src/ReproStore.hs
@@ -1,6 +1,7 @@
 module ReproStore where
 
 import Data.Map.Strict (Map)
+import Data.Set (Set)
 import Data.Text (Text)
 import Data.Vector (Vector)
 
@@ -9,6 +10,8 @@ import Idem (Idem)
 
 -- | Opaque type
 newtype StorePath s = StorePath Text
+
+newtype StoreDerivation s = StoreDerivation Text
 
 pathAsString :: StorePath s -> Text
 pathAsString = undefined
@@ -19,14 +22,30 @@ pathAlreadyExists = undefined
 addToStore :: FSO -> Vector (StorePath s) -> Text -> Idem s (StorePath s)
 addToStore = undefined
 
-data Derivation s = Derivation
-  { attributes :: Map String String
-  , drvPath :: StorePath s
-  , outPath :: StorePath s
+data EnvVal s
+  = EnvValLit Text
+  | EnvValPath (StorePath s)
+
+data DerivationSpecification s = DerivationSpecification
+  { system :: Text
+  , builder :: StorePath s
+  , environment :: Map Text (EnvVal s)
+  , args :: Vector (EnvVal s)
+  , outputs :: Set Text
+  , inputDrvs :: Map (StoreDerivation s) (Vector Text)
+  , inputSrcs :: Set (StorePath s)
   }
 
-derivation :: Map String String -> Idem s (Derivation s)
-derivation = undefined
+instantiate :: DerivationSpecification s -> Idem s (StoreDerivation s)
+instantiate = undefined
 
-buildDerivation :: StorePath s -> Idem s ()
-buildDerivation = undefined
+derivationSpecification ::
+  StoreDerivation s ->
+  Idem s (DerivationSpecification s)
+derivationSpecification = undefined
+
+derivationOutputs :: StoreDerivation s -> Idem s (Map Text (StorePath s))
+derivationOutputs = undefined
+
+build :: StoreDerivation s -> Idem s ()
+build = undefined
